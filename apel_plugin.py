418788acb5384a72287329bc1132ae8e192eee7c
Dirk Sammel <dirk.sammel@cern.ch>
2022-07-11 16:56:46 +0200

    added check for time since last report, switched to asyncio.run(), added while-loop and asyncio.sleep for infinite running

--- apel_plugin.py
+++ apel_plugin.py
@@ -15,7 +15,7 @@ async def get_records():
     return response
 
 async def get_records_since(start_time):
-    response = await client.get_started_since(start_time)
+    response = await client.get_stopped_since(start_time)
     return response
 
 async def create_time_db(time_db_path):
@@ -40,18 +40,29 @@ async def create_time_db(time_db_path):
         print(e)
 
 async def get_start_time(time_db_path):
+    conn = sqlite3.connect(time_db_path)
+    cur = conn.cursor()
+    cur.row_factory = lambda cursor, row: row[0]
+    cur.execute('SELECT last_end_time FROM times')
+    start_time = cur.fetchall()[0]
+    cur.close()
+    conn.close()
+
+    return start_time
+
+async def get_report_time(time_db_path):
     if Path(time_db_path).is_file():
         conn = sqlite3.connect(time_db_path)
         cur = conn.cursor()
         cur.row_factory = lambda cursor, row: row[0]
-        cur.execute('SELECT last_end_time FROM times')
-        start_time = cur.fetchall()[0]
+        cur.execute('SELECT last_report_time FROM times')
+        report_time = cur.fetchall()[0]
         cur.close()
         conn.close()
     else:
-        start_time = await create_time_db(time_db_path)
+        report_time = await create_time_db(time_db_path)
 
-    return start_time
+    return report_time
 
 async def update_time_db(stop_time, report_time, time_db_path):
     update_sql = ''' UPDATE times
@@ -65,36 +76,54 @@ async def update_time_db(stop_time, report_time, time_db_path):
     cur.close()
     conn.close()
 
-async def main(client: AuditorClient, time_db_path):
-    await client.start()
+async def main(client: AuditorClient, time_db_path, run_interval, report_interval):
+    while True:
+        last_report_time = await get_report_time(time_db_path)
+        last_report_time = datetime.strptime(last_report_time,'%Y-%m-%dT%H:%M:%SZ')
+        current_time = datetime.now()
 
-    start_time = await get_start_time(time_db_path)
-    print(start_time)
-    records = await get_records_since(start_time)
-    try:
-        latest_stop_time = records[-1]['stop_time']
-    except:
-        latest_stop_time = start_time
-        print(len(records))
-    #await send_report()
-    latest_report_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-    print(latest_report_time)
-    await update_time_db(latest_stop_time, latest_report_time, time_db_path)
+        print((current_time-last_report_time).total_seconds())
+
+        if not (current_time-last_report_time).total_seconds() >= report_interval:
+            print('Too soon, do nothing for now!')
+            await asyncio.sleep(run_interval)
+            continue
+        else:
+            print('More than 1 minute since last report, do it again!')
+
+        await client.start()
+
+        start_time = await get_start_time(time_db_path)
+        #    print(start_time)
+        records = await get_records_since(start_time)
+        for r in records:
+            print(r)
+        try:
+            latest_stop_time = records[-1]['stop_time']
+            # report = await create_report(records)
+            # await send_report()
+            print(latest_stop_time)
+        except:
+            latest_stop_time = start_time
+        latest_report_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+        await update_time_db(latest_stop_time, latest_report_time, time_db_path)
+
+        await client.stop()
+        await asyncio.sleep(run_interval)
 
 if __name__ == '__main__':
 
-    logging.basicConfig(level=logging.DEBUG)
-    
-    loop = asyncio.get_event_loop()
-
     client = AuditorClient('10.18.1.64', 8000, num_workers=1, db=None)
     time_db_path = '/work/ws/atlas/ds1034-output/time.db'
+    run_interval = 5
+    report_interval = 60
+
+    logging.basicConfig(level=logging.DEBUG)
 
     try:
-        loop.run_until_complete(main(client,time_db_path))
+        asyncio.run(main(client,time_db_path,run_interval,report_interval))
     except KeyboardInterrupt:
+        print("User abort")
         pass
     finally:
-        loop.run_until_complete(client.stop())
-    loop.close()
-
+        asyncio.run(client.stop())
