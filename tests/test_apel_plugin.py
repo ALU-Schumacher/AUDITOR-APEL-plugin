ca8863da1573b1def15c78bffdbf22233365b365
Dirk Sammel <dirk.sammel@gmail.com>
2022-09-20 16:39:24 +0200

    started with unit tests

---
+++ tests/test_apel_plugin.py
@@ -0,0 +1,16 @@
+import pytest
+import apel_plugin
+
+
+class TestAPELPlugin(object):
+    @pytest.mark.asyncio
+    async def test_regex_dict_lookup(self):
+        term_a = "apple"
+        term_b = "banana"
+        dict = {"^a": "apple_in_dict", "^b": "banana_in_dict"}
+
+        result = await apel_plugin.regex_dict_lookup(term_a, dict)
+        assert result == "apple_in_dict"
+
+        result = await apel_plugin.regex_dict_lookup(term_b, dict)
+        assert result == "banana_in_dict"
