60d92c31c33d346f3d38fc65e7f8cd723af3dd4f
Dirk Sammel <dirk.sammel@gmail.com>
2022-09-23 15:43:56 +0200

    changed code structure, added building with setuptools and pyproject.toml, added unit test for SystemExit

--- tests/test_apel_plugin.py
+++ tests/test_apel_plugin.py
@@ -2,11 +2,13 @@ import pytest
 import apel_plugin.apel_plugin as apel_plugin
 
 
-class TestAPELPlugin(object):
-    @pytest.mark.asyncio
+@pytest.mark.asyncio
+class TestAPELPlugin:
     async def test_regex_dict_lookup(self):
         term_a = "apple"
         term_b = "banana"
+        term_c = "citrus"
+
         dict = {"^a": "apple_in_dict", "^b": "banana_in_dict"}
 
         result = await apel_plugin.regex_dict_lookup(term_a, dict)
@@ -14,3 +16,8 @@ class TestAPELPlugin(object):
 
         result = await apel_plugin.regex_dict_lookup(term_b, dict)
         assert result == "banana_in_dict"
+
+        with pytest.raises(SystemExit) as pytest_error:
+            await apel_plugin.regex_dict_lookup(term_c, dict)
+        assert pytest_error.type == SystemExit
+        assert pytest_error.value.code == 1
