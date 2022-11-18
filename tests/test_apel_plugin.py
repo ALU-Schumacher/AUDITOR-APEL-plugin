dcb728b369c28cb7ab9f23e8b29a943997463f02
Dirk Sammel <dirk.sammel@cern.ch>
2022-11-18 16:05:13 +0100

    add test for get_begin_previous_month

--- tests/test_apel_plugin.py
+++ tests/test_apel_plugin.py
@@ -1,5 +1,7 @@
 import pytest
 from apel_plugin import functions
+from datetime import datetime
+import pytz
 
 
 @pytest.mark.asyncio
@@ -21,3 +23,13 @@ class TestAPELPlugin:
             await functions.regex_dict_lookup(term_c, dict)
         assert pytest_error.type == SystemExit
         assert pytest_error.value.code == 1
+
+    async def test_get_begin_previous_month(self):
+        time_a = datetime(2022, 10, 23, 12, 23, 55)
+        time_b = datetime(1970, 1, 1, 00, 00, 00)
+
+        result = await functions.get_begin_previous_month(time_a)
+        assert result == datetime(2022, 9, 1, 00, 00, 00, tzinfo=pytz.utc)
+
+        result = await functions.get_begin_previous_month(time_b)
+        assert result == datetime(1969, 12, 1, 00, 00, 00, tzinfo=pytz.utc)
