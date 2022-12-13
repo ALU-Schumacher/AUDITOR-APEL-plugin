73e93629cc4f8e7aaa30d08b093aea94ab00aec3
Dirk Sammel <dirk.sammel@cern.ch>
2022-12-13 16:27:59 +0100

    rename module (functions->core), import functions in tests individually

--- scripts/publish.py
+++ scripts/publish.py
@@ -11,18 +11,18 @@ from datetime import datetime
 import pytz
 import configparser
 import base64
-from apel_plugin import functions
+from apel_plugin import core
 
 
 async def run(config, client):
     report_interval = config["intervals"].getint("report_interval")
 
-    token = await functions.get_token(config)
+    token = await core.get_token(config)
     logging.debug(token)
 
     while True:
-        time_db_conn = await functions.get_time_db(config)
-        last_report_time = await functions.get_report_time(time_db_conn)
+        time_db_conn = await core.get_time_db(config)
+        last_report_time = await core.get_report_time(time_db_conn)
         current_time = datetime.now()
         time_since_report = (current_time - last_report_time).total_seconds()
 
@@ -35,51 +35,47 @@ async def run(config, client):
             logging.info("Enough time since last report, create new report")
 
         try:
-            start_time = await functions.get_start_time(time_db_conn)
+            start_time = await core.get_start_time(time_db_conn)
             logging.info(f"Getting records since {start_time}")
             records_summary = await client.get_stopped_since(start_time)
             latest_stop_time = records_summary[-1].stop_time.replace(
                 tzinfo=pytz.utc
             )
             logging.debug(f"Latest stop time is {latest_stop_time}")
-            summary_db = await functions.create_summary_db(
-                config, records_summary
-            )
-            grouped_summary_list = await functions.group_summary_db(summary_db)
-            summary = await functions.create_summary(grouped_summary_list)
+            summary_db = await core.create_summary_db(config, records_summary)
+            grouped_summary_list = await core.group_summary_db(summary_db)
+            summary = await core.create_summary(grouped_summary_list)
             logging.debug(summary)
-            signed_summary = await functions.sign_msg(config, summary)
+            signed_summary = await core.sign_msg(config, summary)
             logging.debug(signed_summary)
             encoded_summary = base64.b64encode(signed_summary).decode("utf-8")
             logging.debug(encoded_summary)
-            payload_summary = await functions.build_payload(encoded_summary)
+            payload_summary = await core.build_payload(encoded_summary)
             logging.debug(payload_summary)
-            post_summary = await functions.send_payload(
+            post_summary = await core.send_payload(
                 config, token, payload_summary
             )
             logging.debug(post_summary.status_code)
 
-            begin_previous_month = await functions.get_begin_previous_month(
+            begin_previous_month = await core.get_begin_previous_month(
                 current_time
             )
             records_sync = await client.get_stopped_since(begin_previous_month)
-            sync_db = await functions.create_sync_db(config, records_sync)
-            grouped_sync_list = await functions.group_sync_db(sync_db)
-            sync = await functions.create_sync(grouped_sync_list)
+            sync_db = await core.create_sync_db(config, records_sync)
+            grouped_sync_list = await core.group_sync_db(sync_db)
+            sync = await core.create_sync(grouped_sync_list)
             logging.debug(sync)
-            signed_sync = await functions.sign_msg(config, sync)
+            signed_sync = await core.sign_msg(config, sync)
             logging.debug(signed_sync)
             encoded_sync = base64.b64encode(signed_sync).decode("utf-8")
             logging.debug(encoded_sync)
-            payload_sync = await functions.build_payload(encoded_sync)
+            payload_sync = await core.build_payload(encoded_sync)
             logging.debug(payload_sync)
-            post_sync = await functions.send_payload(
-                config, token, payload_sync
-            )
+            post_sync = await core.send_payload(config, token, payload_sync)
             logging.debug(post_sync.status_code)
 
             latest_report_time = datetime.now()
-            await functions.update_time_db(
+            await core.update_time_db(
                 time_db_conn, latest_stop_time.timestamp(), latest_report_time
             )
         except IndexError:
