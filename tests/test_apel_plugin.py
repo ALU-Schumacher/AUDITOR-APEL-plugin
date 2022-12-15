cee5e7e26e9593e15b37338bac335c157396aea3
Dirk Sammel <dirk.sammel@cern.ch>
2022-12-15 13:36:20 +0100

    move sql_filter() to group_summary_db(), remove unit test for sql_filter

--- tests/test_apel_plugin.py
+++ tests/test_apel_plugin.py
@@ -2,7 +2,6 @@ import pytest
 from apel_plugin.core import (
     regex_dict_lookup,
     get_begin_previous_month,
-    sql_filter,
     create_time_db,
 )
 from datetime import datetime
@@ -37,58 +36,6 @@ class TestAPELPlugin:
 
         result = await get_begin_previous_month(time_b)
         assert result == datetime(1969, 12, 1, 00, 00, 00, tzinfo=pytz.utc)
-
-    async def test_sql_filter(self):
-        create_table_sql = """
-                           CREATE TABLE IF NOT EXISTS records(
-                           site TEXT NOT NULL,
-                           year INTEGER NOT NULL,
-                           month INTEGER NOT NULL
-                           )
-                           """
-
-        insert_record_sql = """
-                            INSERT INTO records(
-                               site,
-                               year,
-                               month
-                           )
-                           VALUES(
-                               ?, ?, ?
-                           )
-                           """
-
-        conn = await aiosqlite.connect(":memory:")
-        cur = await conn.cursor()
-        await cur.execute(create_table_sql)
-
-        site_list = ["site_a", "site_b", "site_c"]
-        year_list = [2020, 2021, 2022]
-        month_list = [1, 7, 12]
-
-        for site in site_list:
-            for year in year_list:
-                for month in month_list:
-                    data_tuple = (site, year, month)
-                    await cur.execute(insert_record_sql, data_tuple)
-        await conn.commit()
-        await cur.close()
-
-        for site in site_list:
-            for year in year_list:
-                for month in month_list:
-                    dst = await aiosqlite.connect(":memory:")
-                    await conn.backup(dst)
-                    filtered_db = await sql_filter(dst, month, year, site)
-                    cur = await filtered_db.cursor()
-                    await cur.execute("SELECT * FROM records")
-                    result = await cur.fetchall()
-                    await cur.close()
-                    await dst.close()
-
-                    assert result == [(site, year, month)]
-
-        await conn.close()
 
     async def test_create_time_db(self):
         path = ":memory:"
