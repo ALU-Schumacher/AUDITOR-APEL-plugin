699d2e7feae9a4899a821d74cef51013fa7b5428
Dirk Sammel <dirk.sammel@cern.ch>
2023-06-07 16:29:15 +0200

    remove version from metdata, add fetch-depth

--- src/apel_plugin/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("apel_plugin")
-except PackageNotFoundError:
-    # package is not installed
-    pass
